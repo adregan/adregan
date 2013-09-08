4af2e717b6d8598e6f39235365ef3afaa482e79a
adregan <duncanregan@gmail.com>
2013-09-08 17:17:42 +0900

    finishing UI

--- src/js/js.js
+++ src/js/js.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
-    $(".project img").click(function(){
-      $(this).next().slideToggle( 200 );
-    })
+
+    $(".info-link").click(function(){
+    if($(this).children(".ex-button").hasClass("rotate")) {
+    $(this).children(".ex-button").removeClass("rotate").addClass("rotate-back");
+    $(this).prev().slideUp('350');
+    } else{
+    $(this).children(".ex-button").addClass("rotate");
+    $(this).prev().slideDown(350);
+    }
+  })
+
 });
